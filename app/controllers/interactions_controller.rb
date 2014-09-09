a3eacc3110f3b9ea3fbaea69463ca6ed62ef0883
Nicolás Santángelo <santangelonicolas@gmail.com>
2014-09-09 02:14:32 -0300

    First controller implementation

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -13,15 +13,36 @@ class InteractionsController < ApplicationController
   # POST /interactions
   # POST /interactions.json
   def create
-    @interaction = Interaction.create(params[:interactions])
+    raise 'No data received' if params[:data].nil?
+
+    remote_object = JSON.parse(params[:data])
+
+    User.find_or_create_by(remote_id: remote_object[:userId], name: remote_object[:userName]) do |user|
+      user.last_name = remote_object[:userLastName]
+      user.birthdate = remote_object[:userBirthdate]
+    end
+
+    Card.find_or_create_by(name: remote_object[:cardName])
+    
+    Receiver.find_or_create_by(name: remote_object[:receiverName]) do |receiver|
+      receiver.last_name = remote_object[:receiverLastName]
+      receiver.relationship = remote_object[:relationship] # Another model?
+    end
+
+    Interaction.create({
+      #card_id: card.id
+      #user_id: user.id
+      #receiver_id: receiver.id
+      level: remote_object[:level]
+    })
 
     respond_to do |format|
-      if @interaction.save
-        PrivatePub.publish_to '/interactions/new', interaction: @interaction.to_json
+      if true #@interaction.save
+        PrivatePub.publish_to '/interactions/new', some_data: 2
 
-        format.json { render json: @interaction, status: :created, location: @interaction }
+        format.json { render json: { success: true }, status: :created }
       else
-        format.json { render json: @interaction.errors, status: :unprocessable_entity }
+        format.json { render json: { errors: "something bad happened" }, status: :unprocessable_entity }
       end
     end
   end
--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -1,3 +1,10 @@
 class Interaction < ActiveRecord::Base
+  fields do
+    receptor :string
+    picture :string
+    level :integer
+    timestamps
+  end
+  
   attr_accessible :receptor, :picture
 end
