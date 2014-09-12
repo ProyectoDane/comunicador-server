e67905b64f5d9dd135ac5cdac680e505131fe16f
Nicolás Santángelo <santangelonicolas@gmail.com>
2014-09-11 23:08:46 -0300

    hotfix

--- db/migrate/20140909050742_create_cards.rb
+++ db/migrate/20140909050742_create_cards.rb
@@ -3,15 +3,9 @@ class CreateCards < ActiveRecord::Migration
     create_table :cards do |t|
       t.string :name
     end
-
-    change_column :interactions, :created_at, :datetime, :null => true
-    change_column :interactions, :updated_at, :datetime, :null => true
   end
 
   def self.down
-    change_column :interactions, :created_at, :datetime, :null => false
-    change_column :interactions, :updated_at, :datetime, :null => false
-
     drop_table :cards
   end
 end
